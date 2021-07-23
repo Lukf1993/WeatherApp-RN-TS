64d253bb061054fb060bbcc0fc12c1e3da8fc7ca
Lukasz <lfundament@netizens.pl>
2021-07-23 15:14:52 +0200

    add apiContext for searching components

--- src/components/atoms/SearchedItem.tsx
+++ src/components/atoms/SearchedItem.tsx
@@ -1,40 +1,15 @@
 import React, { FC } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 type Props = {
-  city: string;
-  country: string;
-  localTime: string;
-  temperatureC: string;
-  temperatureF: string;
-  icon: string;
+  name: string;
 };
 
-const SearchedItem: FC<Props> = ({
-  city,
-  country,
-  localTime,
-  temperatureC,
-  temperatureF,
-  icon,
-}) => {
+const SearchedItem: FC<Props> = ({ name }) => {
   return (
     <View style={styles.wrapper}>
-      <View style={styles.flex}>
-        <Text style={styles.font30}>{city}</Text>
-        <Text style={styles.font20}>{country}</Text>
-      </View>
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: `https:${icon}`,
-        }}
-      />
       <View>
-        <Text style={styles.font40}>
-          {temperatureC}C/{temperatureF}F
-        </Text>
-        <Text style={styles.font20}>{localTime}</Text>
+        <Text style={styles.font30}>{name}</Text>
       </View>
     </View>
   );
@@ -42,8 +17,6 @@ const SearchedItem: FC<Props> = ({
 
 const styles = StyleSheet.create({
   wrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
     borderStyle: 'solid',
     borderRightWidth: 1,
     borderLeftWidth: 1,
@@ -52,21 +25,8 @@ const styles = StyleSheet.create({
     marginRight: 15,
     marginLeft: 15,
   },
-  font40: {
-    fontSize: 40,
-  },
   font30: {
     fontSize: 30,
-  },
-  font20: {
-    fontSize: 20,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  flex: {
-    flex: 1,
   },
 });
 
--- src/components/modules/SearchInput.tsx
+++ src/components/modules/SearchInput.tsx
@@ -1,57 +1,22 @@
 import React, { FC } from 'react';
-import { StyleSheet, View, TextInput, FlatList } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
+import { useApiContext } from '~context/apiContext';
 
 import SearchedItem from '~components/atoms/SearchedItem';
-
-const DATA = [
-  {
-    city: 'Londyn',
-    country: 'Anglia',
-    localTime: '2021-07-22 15:24',
-    temperatureC: '20',
-    temperatureF: '200',
-    icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
-  },
-  {
-    city: 'Katowice',
-    country: 'Polska',
-    localTime: '2021-07-22 15:24',
-    temperatureC: '20',
-    temperatureF: '200',
-    icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
-  },
-  {
-    city: 'Berlin',
-    country: 'Niemcy',
-    localTime: '2021-07-22 15:24',
-    temperatureC: '20',
-    temperatureF: '200',
-    icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
-  },
-];
 
 type Props = {
   [key: string]: any;
 };
+type Context = Props;
 
-const RenderItem: FC<Props> = ({ item }) => {
-  return (
-    <SearchedItem
-      city={item.city}
-      country={item.country}
-      localTime={item.localTime}
-      temperatureC={item.temperatureC}
-      temperatureF={item.temperatureF}
-      icon={item.icon}
-    />
-  );
-};
-
-const SearchInput = () => {
+const SearchInput: FC<Props> = () => {
+  const apiContext: Context = useApiContext();
   return (
     <View>
       <TextInput style={styles.searchInput} placeholder="City" />
-      <FlatList data={DATA} renderItem={RenderItem} />
+      {apiContext.searchData.map((item: Props) => (
+        <SearchedItem name={item.name} />
+      ))}
     </View>
   );
 };
@@ -64,23 +29,6 @@ const styles = StyleSheet.create({
     marginRight: 15,
     marginLeft: 15,
     padding: 15,
-    fontSize: 20,
-  },
-  ListItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderStyle: 'solid',
-    borderRightWidth: 1,
-    borderLeftWidth: 1,
-    borderBottomWidth: 1,
-    padding: 15,
-    marginRight: 15,
-    marginLeft: 15,
-  },
-  city: {
-    fontSize: 30,
-  },
-  font20: {
     fontSize: 20,
   },
 });
