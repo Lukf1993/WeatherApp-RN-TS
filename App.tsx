7ce9a6b00cb3951c49d831e2e86c424984d8c0e5
Lukasz <lfundament@netizens.pl>
2021-07-23 14:10:07 +0200

    add context provider to app

--- App.tsx
+++ App.tsx
@@ -1,30 +1,15 @@
 import React, { FC } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-import MainView from '~screens/MainView';
+import { ApiContextProvider } from '~context/apiContext';
+// import MainView from '~screens/MainView';
 import CityView from '~screens/CityView';
+// import FavoriteView from '~screens/FavoriteView';
 
 const App: FC = () => {
   return (
-    <CityView />
-    // <MainView />
-    // <SafeAreaView>
-    //   <ScrollView contentInsetAdjustmentBehavior="automatic">
-    //     <View>
-    //       <Text style={styles.title}>Choose your city</Text>
-
-    //     </View>
-    //   </ScrollView>
-    // </SafeAreaView>
+    <ApiContextProvider>
+      <CityView />
+    </ApiContextProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    marginTop: 20,
-    fontSize: 40,
-    fontWeight: '600',
-    textAlign: 'center',
-  },
-});
 
 export default App;
