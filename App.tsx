27ea82874de1ef4830cd9a86ab24974e18766304
Lukasz <lfundament@netizens.pl>
2021-07-26 14:35:33 +0200

    add tabs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,39 @@
 import React, { FC } from 'react';
 import { ApiContextProvider } from '~context/apiContext';
-// import MainView from '~screens/MainView';
-import CityView from '~screens/CityView';
-// import FavoriteView from '~screens/FavoriteView';
+import MainView from '~screens/MainView';
+// import CityView from '~screens/CityView';
+import FavoriteView from '~screens/FavoriteView';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return <MainView />;
+}
+function Favorite() {
+  return <FavoriteView />;
+}
 
 const App: FC = () => {
   return (
     <ApiContextProvider>
-      <CityView />
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Home"
+          tabBarOptions={{
+            labelStyle: {
+              fontSize: 20,
+            },
+            style: {
+              height: 50,
+              paddingBottom: 15,
+            },
+          }}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Favorite" component={Favorite} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ApiContextProvider>
   );
 };
