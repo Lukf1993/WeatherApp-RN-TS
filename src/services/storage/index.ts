378fcb29e6716bc7bbc463267207c23f509a675e
Lukasz <lfundament@netizens.pl>
2021-10-11 15:18:01 +0200

    add async storage

---
+++ src/services/storage/index.ts
@@ -0,0 +1,9 @@
+import Storage from 'react-native-storage';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const storage = new Storage({
+    storageBackend: AsyncStorage,
+    defaultExpires: null,
+});
+
+export default storage;
